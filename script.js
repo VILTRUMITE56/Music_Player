8074163b85389e3b19fc701875a90a727964906a
Gabriel Ybanez <ybagabrielfrancis@gmail.com>
2025-08-12 02:15:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,8 @@ const playSong = (id) => {
   }
   userData.currentSong = song;
   playButton.classList.add("playing");
-
+  highlightCurrentSong();
+  setPlayerDisplay();
   audio.play();
 };
 
@@ -127,11 +128,27 @@ const playPreviousSong = () => {
   }
 };
 
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
 const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
   );
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+  if (songToHighlight) {
+    songToHighlight.setAttribute("aria-current", "true");
+  }
 };
 
 const renderSongs = (array) => {
@@ -155,6 +172,8 @@ const renderSongs = (array) => {
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+const setPlayButtonAccessibleText = () => {};
 
 const getCurrentSongIndex = () => {
   return userData?.songs.indexOf(userData?.currentSong);
